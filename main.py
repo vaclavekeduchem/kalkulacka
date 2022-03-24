7d4141c62b240afb8fee89b607115108fb23fbbd
Petr Václavek <vaclavek@educhem.cz>
2022-03-24 10:23:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,33 +20,33 @@ def main():
 
     # 1 row
     one = tk.Button(btn_frame, text="1", font="Arial 20", border=0, padx=15, command=lambda: handle_click("1"), bg="#4ECDC4", activebackground="#1B8A83")
-    one.grid(row=3, column=0)
+    one.grid(row=3, column=0, sticky="nswe")
 
     two = tk.Button(btn_frame, text="2", font="Arial 20", border=0, padx=15, command=lambda: handle_click("2"), bg="#4ECDC4", activebackground="#1B8A83")
-    two.grid(row=3, column=1)
+    two.grid(row=3, column=1, sticky="nswe")
 
     three = tk.Button(btn_frame, text="3", font="Arial 20", border=0, padx=15, command=lambda: handle_click("3"), bg="#4ECDC4", activebackground="#1B8A83")
-    three.grid(row=3, column=2)
+    three.grid(row=3, column=2, sticky="nswe")
 
     # 2 row
     four = tk.Button(btn_frame, text="4", font="Arial 20", border=0, padx=15, command=lambda: handle_click("4"), bg="#4ECDC4", activebackground="#1B8A83")
-    four.grid(row=2, column=0)
+    four.grid(row=2, column=0, sticky="nswe")
 
     five = tk.Button(btn_frame, text="5", font="Arial 20", border=0, padx=15, command=lambda: handle_click("5"), bg="#4ECDC4", activebackground="#1B8A83")
-    five.grid(row=2, column=1)
+    five.grid(row=2, column=1, sticky="nswe")
 
     six = tk.Button(btn_frame, text="6", font="Arial 20", border=0, padx=15, command=lambda: handle_click("6"), bg="#4ECDC4", activebackground="#1B8A83")
-    six.grid(row=2, column=2)
+    six.grid(row=2, column=2, sticky="nswe")
 
     # 3 row
     seven = tk.Button(btn_frame, text="7", font="Arial 20", border=0, padx=15, command=lambda: handle_click("7"), bg="#4ECDC4", activebackground="#1B8A83")
-    seven.grid(row=1, column=0)
+    seven.grid(row=1, column=0, sticky="nswe")
 
     eight = tk.Button(btn_frame, text="8", font="Arial 20", border=0, padx=15, command=lambda: handle_click("8"), bg="#4ECDC4", activebackground="#1B8A83")
-    eight.grid(row=1, column=1)
+    eight.grid(row=1, column=1, sticky="nswe")
 
     nine = tk.Button(btn_frame, text="9", font="Arial 20", border=0, padx=15, command=lambda: handle_click("9"), bg="#4ECDC4", activebackground="#1B8A83")
-    nine.grid(row=1, column=2)
+    nine.grid(row=1, column=2, sticky="nswe")
 
     #zero
     zero = tk.Button(btn_frame, text="0", font="Arial 20", border=0, padx=15, command=lambda: handle_click("0"), bg="#4ECDC4", activebackground="#1B8A83")
@@ -63,6 +63,15 @@ def main():
 
     minus = tk.Button(btn_frame, text="-", font="Arial 20", border=0, padx=15, command=lambda: handle_click("-"), bg="#4ECDC4", activebackground="#1B8A83")
     minus.grid(row=0, column=3, sticky="nswe")
+
+    mupltiply = tk.Button(btn_frame, text="*", font="Arial 20", border=0, padx=15, command=lambda: handle_click("*"), bg="#4ECDC4", activebackground="#1B8A83")
+    mupltiply.grid(row=0, column=2, sticky="nswe")
+
+    divide = tk.Button(btn_frame, text="/", font="Arial 20", border=0, padx=15, command=lambda: handle_click("/"), bg="#4ECDC4", activebackground="#1B8A83")
+    divide.grid(row=0, column=1, sticky="nswe")
+
+    clear = tk.Button(btn_frame, text="C", font="Arial 20", border=0, padx=15, bg="#4ECDC4", activebackground="#1B8A83")
+    clear.grid(row=0, column=0)
 
     okno.mainloop()
 
